dfc0e45673c666c1009189b06a8be09a38adbd17
i7p7x0 <i7p7x0@gmail.com>
2022-09-17 16:41:27 +0545

    Completed section 1-4

---
+++ Tangy/Tangy_Models/LearnBlazorModels/Demo_ProductProp.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Tangy_Models.LearnBlazorModels;
+
+	public class Demo_ProductProp
+	{
+		public int Id { get; set; }
+
+		public string Key { get; set; } = String.Empty;
+
+		public string Value { get; set; } = String.Empty;
+	}
+
