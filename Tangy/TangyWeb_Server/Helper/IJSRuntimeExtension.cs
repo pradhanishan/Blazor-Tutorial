cb0f1b0b3a68ffc521dd57d11ba2e32e1a650d7b
i7p7x0 <i7p7x0@gmail.com>
2022-09-18 09:14:23 +0545

    Completed section 6

---
+++ Tangy/TangyWeb_Server/Helper/IJSRuntimeExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace TangyWeb_Server.Helper
+{
+	public static class IJSRuntimeExtension
+	{
+		public static async ValueTask ToastrSuccess(this IJSRuntime jSRuntime, string message)
+		{
+            await jSRuntime.InvokeVoidAsync("ShowToastr", "success", message);
+        }
+
+        public static async ValueTask ToastrError(this IJSRuntime jSRuntime, string message)
+        {
+            await jSRuntime.InvokeVoidAsync("ShowToastr", "error", message);
+        }
+    }
+}
---
+++ Tangy/TangyWeb_Server/wwwroot/js/common.js
@@ -0,0 +1,24 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operation Successful", { timeOut: 5000 });
+    } if (type === "error") {
+        toastr.error(message, "Operation Failed", { timeOut: 5000 });
+    }
+}
+
+window.ShowSweetAlert = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Operation Successful!',
+            message,
+            type
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Operation Failed!',
+            message,
+            type
+        )
+    }
+}
