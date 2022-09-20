fde850b6a303a1248d5077e9aae1c2cf7aecb60e
i7p7x0 <i7p7x0@gmail.com>
2022-09-20 10:25:56 +0545

    Completed Section 14

--- Tangy/TangyWeb_API/Program.cs
+++ Tangy/TangyWeb_API/Program.cs
@@ -17,6 +17,10 @@ builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped<IProductPriceRepository, ProductPriceRepository>();
+builder.Services.AddCors(x => x.AddPolicy("Tangy", builder =>
+{
+    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+}));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,6 +31,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseCors("Tangy");
 app.UseRouting();
 app.UseAuthorization();
 
---
+++ Tangy/TangyWeb_Client/wwwroot/js/common.js
@@ -0,0 +1,32 @@
+﻿window.ShowToastr = (type, message) => {
+    if (type === "success") {
+        toastr.success(message, "Operation Successful", { timeOut: 5000 });
+    } if (type === "error") {
+        toastr.error(message, "Operation Failed", { timeOut: 5000 });
+    }
+}
+
+window.ShowSweetAlert = (type, message) => {
+    if (type === "success") {
+        Swal.fire(
+            'Operation Successful!',
+            message,
+            type
+        )
+    }
+    if (type === "error") {
+        Swal.fire(
+            'Operation Failed!',
+            message,
+            type
+        )
+    }
+}
+
+function ShowDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('show');
+}
+
+function HideDeleteConfirmationModal() {
+    $('#deleteConfirmationModal').modal('hide');
+}
