3500a72d4a827b0e1d2852cec92a27b256036060
i7p7x0 <i7p7x0@gmail.com>
2022-09-21 10:45:43 +0545

    Completed section 15

---
+++ Tangy/TangyWeb_Client/Service/CartService.cs
@@ -0,0 +1,75 @@
+﻿using Blazored.LocalStorage;
+using Tangy_Common;
+using TangyWeb_Client.Service.IService;
+using TangyWeb_Client.ViewModels;
+
+namespace TangyWeb_Client.Service
+{
+    public class CartService : ICartService
+    {
+        private readonly ILocalStorageService _localStorage;
+        public event Action OnChange;
+        public CartService(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public async Task DecrementCart(ShoppingCartVM cartToDecrement)
+        {
+            var cart = await _localStorage.GetItemAsync<List<ShoppingCartVM>>(SD.ShoppingCart);
+
+            for (int i = 0; i < cart.Count; i++)
+            {
+                if (cart[i].ProductId == cartToDecrement.ProductId && cart[i].ProductPriceId == cartToDecrement.ProductPriceId)
+                {
+                    if (cart[i].Count == 1 || cartToDecrement.Count == 0)
+                    {
+                        cart.Remove(cart[i]);
+                    }
+                    else
+                    {
+                        cart[i].Count -= cartToDecrement.Count;
+                    }
+                }
+
+            }
+            await _localStorage.SetItemAsync(SD.ShoppingCart, cart);
+            OnChange.Invoke();
+
+
+        }
+
+        public async Task IncrementCart(ShoppingCartVM cartToAdd)
+        {
+            var cart = await _localStorage.GetItemAsync<List<ShoppingCartVM>>(SD.ShoppingCart);
+            bool itemInCart = false;
+            if (cart == null)
+            {
+                cart = new List<ShoppingCartVM>();
+            }
+
+            foreach (var obj in cart)
+            {
+                if (obj.ProductId == cartToAdd.ProductId && obj.ProductPriceId == cartToAdd.ProductPriceId)
+                {
+                    itemInCart = true;
+                    obj.Count += cartToAdd.Count;
+                }
+            }
+
+            if (!itemInCart)
+            {
+                cart.Add(new ShoppingCartVM()
+                {
+                    ProductId = cartToAdd.ProductId,
+                    ProductPriceId = cartToAdd.ProductPriceId,
+                    Count = cartToAdd.Count
+                });
+            }
+
+            await _localStorage.SetItemAsync(SD.ShoppingCart, cart);
+            OnChange.Invoke();
+
+        }
+    }
+}
---
+++ Tangy/TangyWeb_Client/Service/IService/ICartService.cs
@@ -0,0 +1,11 @@
+﻿using TangyWeb_Client.ViewModels;
+
+namespace TangyWeb_Client.Service.IService
+{
+    public interface ICartService
+    {
+        public event Action OnChange;
+        Task DecrementCart(ShoppingCartVM shoppingCart);
+        Task IncrementCart(ShoppingCartVM shoppingCart);
+    }
+}
